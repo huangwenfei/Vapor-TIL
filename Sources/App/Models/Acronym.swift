00054e0138975010296c6d1a3f2cc06828092618
huangwenfei <yi.yuan.zi@163.com>
2019-01-17 18:04:29 +0800

    first commit

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,34 @@
+
+import Vapor
+import FluentSQLite
+
+final class Acronym: Codable {
+    
+    var id : Int?
+    var short : String
+    var long : String
+    
+    init(short: String, long: String) {
+        self.short = short
+        self.long  = long
+    }
+    
+}
+
+extension Acronym: Model {
+    
+    typealias Database = SQLiteDatabase
+    
+    typealias ID = Int
+    
+    public static var idKey: IDKey = \Acronym.id
+    
+//    public static var idKey: WritableKeyPath<Acronym, Int?> {
+//        return \Acronym.id
+//    }
+    
+}
+
+extension Acronym: Migration {}
+
+extension Acronym: Content {}
